8f3d461d102f6a3384d95472478b24ef1bd9a7a5
Jojatekok <jojatekokweb@gmail.com>
2012-07-14 10:32:32 +0200

    Added variables for most of the messages
    And marked messages that don't have any arguments.

--- EECloudAPI/Message.vb
+++ EECloudAPI/Message.vb
@@ -9,6 +9,7 @@
 
     Public Class GroupDisallowedJoin_Message
         Inherits Message
+        'No arguments
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -17,6 +18,7 @@
 
     Public Class Upgrade_Message
         Inherits Message
+        'No arguments
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -30,6 +32,7 @@
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
+
             Title = PMessage.Item(0)
             Text = PMessage.Item(1)
         End Sub
@@ -180,6 +183,8 @@
 
     Public Class SayOld_Message
         Inherits Message
+        Private ReadOnly UserName As String '0
+        Private ReadOnly Text As String '1
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -188,6 +193,8 @@
 
     Public Class AutoText_Message
         Inherits Message
+        Private ReadOnly UserID As Integer '0
+        Private ReadOnly Text As String '1
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -196,6 +203,8 @@
 
     Public Class Write_Message
         Inherits Message
+        Private ReadOnly Title As String '0
+        Private ReadOnly Text As String '1
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -204,6 +213,10 @@
 
     Public Class BlockPlace_Message
         Inherits Message
+        Private ReadOnly Layer As Integer '0
+        Private ReadOnly PosX As Integer '1
+        Private ReadOnly PosY As Integer '2
+        Private ReadOnly BlockID As Integer '4
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -212,6 +225,10 @@
 
     Public Class CoinDoorPlace_Message
         Inherits Message
+        Private ReadOnly PosX As Integer '0
+        Private ReadOnly PosY As Integer '1
+        Private ReadOnly BlockID As Integer '2
+        Private ReadOnly CoinsToOpen As Integer '3
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -220,6 +237,10 @@
 
     Public Class SoundPlace_Message
         Inherits Message
+        Private ReadOnly PosX As Integer '0
+        Private ReadOnly PosY As Integer '1
+        Private ReadOnly BlockID As Integer '2
+        Private ReadOnly SoundID As Integer '3
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -228,6 +249,12 @@
 
     Public Class PortalPlace_Message
         Inherits Message
+        Private ReadOnly PosX As Integer '0
+        Private ReadOnly PosY As Integer '1
+        Private ReadOnly BlockID As Integer '2
+        Private ReadOnly Rotation As Integer '3
+        Private ReadOnly ID As Integer '4
+        Private ReadOnly Target As Integer '5
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -236,6 +263,10 @@
 
     Public Class LabelPlace_Message
         Inherits Message
+        Private ReadOnly PosX As Integer '0
+        Private ReadOnly PosY As Integer '1
+        Private ReadOnly BlockID As Integer '2
+        Private ReadOnly Text As String '3
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -244,6 +275,8 @@
 
     Public Class Godmode_Message
         Inherits Message
+        Private ReadOnly UserID As Integer '0
+        Private ReadOnly IsGod As Boolean '1
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -252,6 +285,7 @@
 
     Public Class Modmode_Message
         Inherits Message
+        Private ReadOnly UserID As Integer '0
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -260,6 +294,7 @@
 
     Public Class Access_Message
         Inherits Message
+        'No arguments
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -268,6 +303,7 @@
 
     Public Class LostAccess_Message
         Inherits Message
+        'No arguments
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -276,14 +312,28 @@
 
     Public Class Teleport_Message
         Inherits Message
+        'Private ReadOnly ? As Boolean '0
+        Private ReadOnly UserIDs() As Integer '1; (4; 7; 10; ...)
+        Private ReadOnly SpawnPosXs() As Integer '2; (5; 8; 11; ...)
+        Private ReadOnly SpawnPosYs() As Integer '3; (6; 9; 12; ...)
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
+
+            ReDim UserIDs((PMessage.Count - 4) / 3)
+            ReDim SpawnPosXs((PMessage.Count - 4) / 3)
+            ReDim SpawnPosYs((PMessage.Count - 4) / 3)
+            For N = 1 To PMessage.Count - 1 Step 3
+                UserIDs(N - 1) = PMessage.Item(N)
+                SpawnPosXs(N - 1) = PMessage.Item(N + 1)
+                SpawnPosYs(N - 1) = PMessage.Item(N + 2)
+            Next
         End Sub
     End Class
 
     Public Class Reset_Message
         Inherits Message
+        Private ReadOnly RoomData As PlayerIOClient.Message
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -292,6 +342,8 @@
 
     Public Class Clear_Message
         Inherits Message
+        Private ReadOnly RoomWidth As Integer '0
+        Private ReadOnly RoomHeight As Integer '1
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
@@ -300,6 +352,7 @@
 
     Public Class SaveDone_Message
         Inherits Message
+        'No arguments
 
         Public Sub New(PMessage As PlayerIOClient.Message)
             MyBase.New(PMessage)
