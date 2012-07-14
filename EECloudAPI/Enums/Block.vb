64cee8193dd8c277fa4f719f4a9bcb800ea12f92
Jojatekok <jojatekokweb@gmail.com>
2012-07-14 21:37:05 +0200

    Added some Block enumerators

---
+++ EECloudAPI/Enums/Block.vb
@@ -0,0 +1,30 @@
+﻿Public Enum Block
+    Block_Gravity_Nothing = 0
+    Block_Gravity_Left = 1
+    Block_Gravity_Up = 2
+    Block_Gravity_Right = 3
+    Block_Gravity_Dot = 4
+
+    Block_Crown = 5
+
+    Block_Key_Red = 6
+    Block_Key_Green = 7
+    Block_Key_Blue = 8
+
+    Block_Basic_Grey = 9
+    Block_Basic_DarkBlue = 10
+    Block_Basic_Purple = 11
+    Block_Basic_Red = 12
+    Block_Basic_Yellow = 13
+    Block_Basic_Green = 14
+    Block_Basic_LightBlue = 15
+
+    Block_Brick_DarkBrown = 16
+    Block_Brick_DarkGreen = 17
+    Block_Brick_Purple = 18
+    Block_Brick_LightGreen = 19
+    Block_Brick_Red = 20
+    Block_Brick_LightBrown = 21
+
+    Block_Special_22 = 22
+End Enum
