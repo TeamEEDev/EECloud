d75d997426284b7a900d695cd6227a9bf43c71d8
Yonom <sepi1376@yahoo.com>
2012-07-14 16:40:43 +0200

    readded a test project

---
+++ EECloudAPI.Tests/UnitTest1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports EECloudAPI
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub TestMethod1()
+
+    End Sub
+
+End Class
