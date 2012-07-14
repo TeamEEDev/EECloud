35bff4e97a25ae83dbbb8ad6c4e03a26bbe6bea5
Yonom <Yonom>
2012-07-14 16:19:35 +0200

    added EECloud; the connection manager

--- EECloudAPI/Classes/Player.vb
+++
@@ -1,4 +0,0 @@
-﻿Public Class Player
-
-End Class
-
---
+++ EECloudAPI/Modules/EECloud.vb
@@ -0,0 +1,42 @@
+﻿Public Module EECloud
+    Private CloudConnections As New List(Of CloudConnection)
+    Public ReadOnly Property ConnectionsCount As Integer
+        Get
+            Return CloudConnections.Count
+        End Get
+    End Property
+
+    Private m_MainConnection As Integer = 0
+    Public Property MainConnection As CloudConnection
+        Get
+            If CloudConnections.Count >= m_MainConnection + 1 Then
+                Return CloudConnections(m_MainConnection)
+            Else
+                Return Nothing
+            End If
+        End Get
+        Set(value As CloudConnection)
+            If CloudConnections.Contains(value) Then
+                m_MainConnection = CloudConnections.IndexOf(value)
+            Else
+                Throw New ApplicationException("Unknown Connection")
+            End If
+        End Set
+    End Property
+
+    Public Sub AddConnection(PConnection As CloudConnection)
+        If Not CloudConnections.Contains(PConnection) Then
+            CloudConnections.Add(PConnection)
+        Else
+            Throw New ApplicationException("Connection has been already added.")
+        End If
+    End Sub
+
+    Public Sub RemoveConnection(PConnection As CloudConnection)
+        If CloudConnections.Contains(PConnection) Then
+            CloudConnections.Remove(PConnection)
+        Else
+            Throw New ApplicationException("Unknown Connection")
+        End If
+    End Sub
+End Module
