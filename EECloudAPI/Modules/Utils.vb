bc685862c78c1b53de70946b61da12cf2c5ca864
Yonom <sepi1376@yahoo.com>
2012-07-14 15:48:11 +0200

    Removed Cloud Client as it was getting useless

---
+++ EECloudAPI/Modules/Utils.vb
@@ -0,0 +1,7 @@
+﻿Module Utils
+    Friend Const gameID As String = "everybody-edits-su9rn58o40itdbnw69plyw"
+    Friend Const gameVersion As String = "118"
+    Friend Const NormalRoom As String = "Everybodyedits" + gameVersion
+    Friend Const BetaRoom As String = "Beta" + gameVersion
+    Friend Const TutorialRoom As String = "Tutorial" + gameVersion + "_world_"
+End Module
