196f96237565f67d935433277d92a56ec7e4ec17
Yonom <sepi1376@yahoo.com>
2012-07-14 22:15:26 +0200

    Added sendmessages and renamed messages to recivemessages

---
+++ EECloudAPI/Namespaces/Send.vb
@@ -0,0 +1,9 @@
+﻿Namespace Send
+    Public MustInherit Class SendMessage
+        Public ReadOnly Message As PlayerIOClient.Message
+
+        Public Sub New(PMessage As PlayerIOClient.Message)
+            Message = PMessage
+        End Sub
+    End Class
+End Namespace
