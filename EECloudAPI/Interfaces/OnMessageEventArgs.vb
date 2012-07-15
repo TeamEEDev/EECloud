49aed8345301739903efb41a44ccf7b347c5b4e3
Yonom <Yonom>
2012-07-15 20:59:40 +0200

    Renamed MessageType to ReciveType, Added 3 send messages, progress in plugin handeling

--- EECloudAPI/Interfaces/OnMessageEventArgs.vb
+++ EECloudAPI/Interfaces/OnMessageEventArgs.vb
@@ -1,5 +1,5 @@
 ﻿Public MustInherit Class OnMessageEventArgs
     Inherits EventArgs
-    Public MustOverride ReadOnly Property Type As MessageType
+    Public MustOverride ReadOnly Property Type As ReciveType
     Public MustOverride ReadOnly Property Message As Recive.ReciveMessage
 End Class
