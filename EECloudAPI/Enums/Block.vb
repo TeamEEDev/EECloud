0dbaf8614b9657c7ccf9598705fa9eb6c19a5f6d
Jojatekok <jojatekokweb@gmail.com>
2012-07-15 09:31:16 +0200

    Added many more block enums; minor changes

--- EECloudAPI/Enums/Block.vb
+++ EECloudAPI/Enums/Block.vb
@@ -26,5 +26,68 @@
     Block_Brick_Red = 20
     Block_Brick_LightBrown = 21
 
-    Block_Special_22 = 22
+    Block_Special_1 = 22
+    Block_Special_2 = 32
+    Block_Special_NormalBlack = 33
+    Block_Special_FullyBlack = 44
+
+    Block_Door_Red = 23
+    Block_Door_Green = 24
+    Block_Door_Blue = 25
+    Block_Door_Coin = 43
+
+    Block_Gate_Red = 26
+    Block_Gate_Green = 27
+    Block_Gate_Blue = 28
+
+    Block_Metal_White = 29
+    Block_Metal_Red = 30
+    Block_Metal_Yellow = 31
+
+    Block_Grass_1 = 34
+    Block_Grass_2 = 35
+    Block_Grass_3 = 36
+
+    Block_Beta_Pink = 37
+    Block_Beta_Green = 38
+    Block_Beta_Blue = 39
+    Block_Beta_Red = 40
+    Block_Beta_Yellow = 41
+    Block_Beta_Grey = 42
+
+    Block_Factory_1 = 45
+    Block_Factory_2 = 46
+    Block_Factory_3 = 47
+    Block_Factory_4 = 48
+    Block_Factory_5 = 49
+
+    Block_Secret_Unpassable = 50
+    Block_Secret_Passable = 243
+
+    Block_Glass_Red = 51
+    Block_Glass_Pink = 52
+    Block_Glass_Purple = 53
+    Block_Glass_DarkBlue = 54
+    Block_Glass_LightBlue = 55
+    Block_Glass_Green = 56
+    Block_Glass_Yellow = 57
+    Block_Glass_Orange = 58
+
+    Block_Summer2011_Sand = 59
+    Block_Summer2011_Sunshade = 228
+    Decoration_Summer2011_1 = 229
+    Decoration_Summer2011_2 = 230
+    Decoration_Summer2011_Rock = 231
+
+    Block_Candy_SolidPink = 60
+    Block_Candy_OneWayPink = 61
+    Block_Candy_OneWayRed = 62
+    Block_Candy_OneWayBlue = 63
+    Block_Candy_OneWayGreen = 64
+    Block_Candy_SolidStripeyRedWhite = 65
+    Block_Candy_SolidStripeyYellowBlackPink = 66
+    Block_Candy_SolidChocolate = 67
+    Decoration_Candy = 227
+    Background_Candy_Pink = 539
+    Background_Candy_Blue = 540
 End Enum
--- EECloudAPI/Enums/MessageType.vb
+++ EECloudAPI/Enums/MessageType.vb
@@ -36,4 +36,3 @@
     GiveWitch
     GiveGrinch
 End Enum
-
