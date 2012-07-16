488b0651be4583ab2b20898d29b9fc644355dd8f
Yonom <Yonom>
2012-07-16 19:01:04 +0200

    Prepared BlockManager for creation

---
+++ EECloud.Host/Classes/EEBlockManager.vb
@@ -0,0 +1,33 @@
+﻿Public Class EEBlockManager
+    Inherits API.BlockManager
+
+    Public Overrides ReadOnly Property CorrectLayer(PID As Integer, PLayer As Layer) As Layer
+        Get
+
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property IsCoindoor(PID As Integer) As Boolean
+        Get
+
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property IsLabel(PID As Integer) As Boolean
+        Get
+
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property IsPortal(PID As Integer) As Boolean
+        Get
+
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property IsSound(PID As Integer) As Boolean
+        Get
+
+        End Get
+    End Property
+End Class
