bd58c759801d213a68f91667a8179397242fbd19
Yonom <Yonom>
2012-07-16 20:14:58 +0200

    Moved some stuff to the EECloud namespace from EECloud.API

---
+++ EECloud.API/Attributes/PluginAttribute.vb
@@ -0,0 +1,3 @@
+﻿Public Class PluginAttribute
+
+End Class
--- EECloud.API/Enums/Layer.vb
+++ EECloud.API/Enums/Layer.vb
@@ -1,4 +1,7 @@
-﻿Public Enum Layer
-    Foreground = 0
-    Background = 1
-End Enum
+﻿Namespace Global.EECloud
+    Public Enum Layer
+        Foreground = 0
+        Background = 1
+    End Enum
+End Namespace
+
