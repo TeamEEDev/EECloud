1d2cd69020ec3855dbfd0fa4886c783f38d27a93
Jojatekok <jojatekokweb@gmail.com>
2012-07-16 19:25:08 +0200

    Completed EEBlockManager

--- EECloud.Host/Classes/EEBlockManager.vb
+++ EECloud.Host/Classes/EEBlockManager.vb
@@ -3,31 +3,53 @@
 
     Public Overrides ReadOnly Property CorrectLayer(PID As Integer, PLayer As Layer) As Layer
         Get
-
+            If (PID > 0 And PID < 500) Or PID = 1000 Then
+                Return Layer.Foreground
+            ElseIf PID >= 500 And PID < 1000 Then
+                Return Layer.Background
+            Else
+                Return PLayer
+            End If
         End Get
     End Property
 
     Public Overrides ReadOnly Property IsCoindoor(PID As Integer) As Boolean
         Get
-
-        End Get
-    End Property
-
-    Public Overrides ReadOnly Property IsLabel(PID As Integer) As Boolean
-        Get
-
-        End Get
-    End Property
-
-    Public Overrides ReadOnly Property IsPortal(PID As Integer) As Boolean
-        Get
-
+            If PID = 43 Then
+                Return True
+            Else
+                Return False
+            End If
         End Get
     End Property
 
     Public Overrides ReadOnly Property IsSound(PID As Integer) As Boolean
         Get
+            If PID = 77 Or PID = 83 Then
+                Return True
+            Else
+                Return False
+            End If
+        End Get
+    End Property
 
+    Public Overrides ReadOnly Property IsPortal(PID As Integer) As Boolean
+        Get
+            If PID = 242 Then
+                Return True
+            Else
+                Return False
+            End If
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property IsLabel(PID As Integer) As Boolean
+        Get
+            If PID = 1000 Then
+                Return True
+            Else
+                Return False
+            End If
         End Get
     End Property
 End Class
