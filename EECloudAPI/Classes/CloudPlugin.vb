941787d95855d175e24759ca13c54f665f3596bf
Yonom <sepi1376@yahoo.com>
2012-07-16 13:27:09 +0200

    i love commits

--- EECloudAPI/Classes/CloudPlugin.vb
+++ EECloudAPI/Classes/CloudPlugin.vb
@@ -1,11 +1,11 @@
 ﻿Public MustInherit Class CloudPlugin
-
     Private myHost As Interfaces.CloudPluginHost
     Friend Sub AttemptSetup(Host As Interfaces.CloudPluginHost)
         If myHost Is Nothing Then
             myHost = Host
         End If
     End Sub
+
     Public MustOverride Sub OnEnable()
     Public MustOverride Sub OnDisable()
 End Class
