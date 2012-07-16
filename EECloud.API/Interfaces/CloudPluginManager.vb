75a0e26d04b435e87df7bfd617019784c146370a
Yonom <sepi1376@yahoo.com>
2012-07-16 20:25:39 +0200

    Renamed EECloud.Host to HostAPI and EECloud.API to PluginAPI

--- EECloud.API/Interfaces/CloudPluginManager.vb
+++
@@ -1,5 +0,0 @@
-﻿Public MustInherit Class CloudPluginManager
-    Protected Sub DoSetup(Plugin As CloudPlugin(Of EECloud.API.CloudPlayer), Host As Interfaces.CloudPluginHost)
-        Plugin.AttemptSetup(Host)
-    End Sub
-End Class
