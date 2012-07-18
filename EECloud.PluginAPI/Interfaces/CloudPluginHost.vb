0775e461b54c36b60f71a9beeab016840cdbc0dd
Yonom <sepi1376@yahoo.com>
2012-07-18 10:17:57 +0200

    Removed the Interfaces namespace

--- EECloud.PluginAPI/Interfaces/CloudPluginHost.vb
+++ EECloud.PluginAPI/Interfaces/CloudPluginHost.vb
@@ -1,5 +1,3 @@
-﻿Namespace Interfaces
-    Public MustInherit Class CloudPluginHost
-        Public MustOverride ReadOnly Property CloudManager As CloudManager
-    End Class
-End Namespace
+﻿Public MustInherit Class CloudPluginHost
+    Public MustOverride ReadOnly Property CloudManager As CloudManager
+End Class
