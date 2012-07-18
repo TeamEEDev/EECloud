c347a1650a1140eb06d08ddcbe3d7188ee938df1
Yonom <sepi1376@yahoo.com>
2012-07-18 16:37:12 +0200

    Edited build order

--- EECloud.PluginAPI/Modules/Utils.vb
+++ EECloud.PluginAPI/Modules/Utils.vb
@@ -1,3 +1,3 @@
 ﻿Public Module Config
-    Friend Const GameID As String = "everybody-edits-su9rn58o40itdbnw69plyw"
+    Public Const GameID As String = "everybody-edits-su9rn58o40itdbnw69plyw"
 End Module
