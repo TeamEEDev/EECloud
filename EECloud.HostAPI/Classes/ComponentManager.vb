ec0be84f8bd6c74623f57270f7dcc151f21e9fa4
Yonom <Yonom>
2012-07-18 19:58:23 +0200

    First actually working version

---
+++ EECloud.HostAPI/Classes/ComponentManager.vb
@@ -0,0 +1,20 @@
+﻿<Export(GetType(IComponentManager))>
+Public Class ComponentManager
+    Implements EECloud.PluginAPI.IComponentManager
+
+    <Import(GetType(IBlockManager))>
+    Private m_BlockManager As IBlockManager
+    Public ReadOnly Property BlockManager As IBlockManager Implements IComponentManager.BlockManager
+        Get
+            Return m_BlockManager
+        End Get
+    End Property
+
+    <Import(GetType(IConnectionManager))>
+    Private m_ConnectionManager As IConnectionManager
+    Public ReadOnly Property ConnectionManager As IConnectionManager Implements IComponentManager.ConnectionManager
+        Get
+            Return m_ConnectionManager
+        End Get
+    End Property
+End Class
--- EECloud/ModuleMain.vb
+++ EECloud/ModuleMain.vb
@@ -1,8 +1,12 @@
 ﻿Module ModuleMain
 
-    Dim myHost As EECloud.HostAPI.EECloudHost
+    Dim myHost As EECloud.HostAPI.CloudHost
     Sub Main()
-        myHost = New EECloud.HostAPI.EECloudHost
+        myHost = New EECloud.HostAPI.CloudHost
+        If myHost.ComponentManager.BlockManager IsNot Nothing Then
+            Console.WriteLine("Things work, proof: " & myHost.ComponentManager.BlockManager.IsPortal(242) & ", " & myHost.ComponentManager.BlockManager.IsPortal(222))
+        End If
+        Console.ReadLine()
     End Sub
 
 End Module
