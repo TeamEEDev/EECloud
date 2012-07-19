6e216c75b82ac4a15f24b6afa225e6ed3595b0b4
Jojatekok <jojatekokweb@gmail.com>
2012-07-19 19:43:48 +0200

    Better world joining algorithm

--- EECloud.ConnectionManager/Classes/CloudConnection.vb
+++ EECloud.ConnectionManager/Classes/CloudConnection.vb
@@ -46,9 +46,9 @@ Public Class CloudConnection
         End If
     End Sub
 
-    Sub New(PClient As PlayerIOClient.Client, PWorldID As String)
+    Sub New(PClient As PlayerIOClient.Client, PWorldID As String, Optional ByVal PVisible As Boolean = True, Optional ByVal PRoomType As RoomType = ConnectionManager.RoomType.Everybodyedits)
         If PClient IsNot Nothing Then
-            m_Connection = PClient.Multiplayer.JoinRoom(PWorldID, Nothing)
+            CreateJoinRoom(PClient, PWorldID, PVisible, PRoomType)
             m_WorldID = PWorldID
             Init()
         Else
@@ -56,11 +56,25 @@ Public Class CloudConnection
         End If
     End Sub
 
-    Sub New(PUsername As String, PPassword As String, PWorldID As String)
+    Sub New(PUsername As String, PPassword As String, PWorldID As String, Optional ByVal PVisible As Boolean = True, Optional ByVal PRoomType As RoomType = ConnectionManager.RoomType.Everybodyedits)
         Dim myClient As PlayerIOClient.Client = PlayerIOClient.PlayerIO.QuickConnect.SimpleConnect(Config.GameID, PUsername, PPassword)
-        m_Connection = myClient.Multiplayer.JoinRoom(PWorldID, Nothing)
+        CreateJoinRoom(myClient, PWorldID, PVisible, PRoomType)
         m_WorldID = PWorldID
         Init()
+    End Sub
+
+    Private Sub CreateJoinRoom(ByVal PClient As PlayerIOClient.Client, ByVal PWorldID As String, Optional ByVal PVisible As Boolean = True, Optional ByVal PRoomType As RoomType = ConnectionManager.RoomType.Everybodyedits)
+        Try
+            PClient.Multiplayer.CreateJoinRoom("GetEEVersion", "GetEEVersion", False, Nothing, Nothing)
+        Catch Err As PlayerIOClient.PlayerIOError
+            Dim ErrorMessage() As String = Err.Message.Split(CChar(" "))
+            For N = ErrorMessage.Length - 1 To 0 Step -1
+                If ErrorMessage(N).StartsWith("Everybodyedits") Then 'CStr(PRoomType) doesn't work
+                    m_Connection = PClient.Multiplayer.CreateJoinRoom(PWorldID, Replace(ErrorMessage(N), ",", ""), PVisible, Nothing, Nothing)
+                    Exit Sub
+                End If
+            Next
+        End Try
     End Sub
 
     Private Sub Init()
