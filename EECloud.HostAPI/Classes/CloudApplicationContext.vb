c24e3df4fdb6ce66beb008bf0e6e85dd70dba9a3
Yonom <sepi1376@yahoo.com>
2012-07-19 11:29:51 +0200

    Added cloudapplicationcontext

---
+++ EECloud.HostAPI/Classes/CloudApplicationContext.vb
@@ -0,0 +1,17 @@
+﻿Public Class CloudApplicationContext
+    Inherits System.Windows.Forms.ApplicationContext
+    Dim myHost As EECloud.HostAPI.CloudHost
+
+    Public Sub New()
+        Dim PluginsPath As String = My.Application.Info.DirectoryPath & "\Plugins"
+        If Not System.IO.Directory.Exists(PluginsPath) Then
+            System.IO.Directory.CreateDirectory(PluginsPath)
+        End If
+
+        Dim ComponentsPath As String = My.Application.Info.DirectoryPath & "\Components"
+        If Not System.IO.Directory.Exists(ComponentsPath) Then
+            System.IO.Directory.CreateDirectory(ComponentsPath)
+        End If
+        myHost = New EECloud.HostAPI.CloudHost(ComponentsPath)
+    End Sub
+End Class
--- EECloud/ModuleMain.vb
+++ EECloud/ModuleMain.vb
@@ -2,16 +2,7 @@
     Dim myHost As EECloud.HostAPI.CloudHost
 
     Function Main() As Integer
-        Dim PluginsPath As String = My.Application.Info.DirectoryPath & "\Plugins"
-        If Not System.IO.Directory.Exists(PluginsPath) Then
-            System.IO.Directory.CreateDirectory(PluginsPath)
-        End If
-
-        Dim ComponentsPath As String = My.Application.Info.DirectoryPath & "\Components"
-        If Not System.IO.Directory.Exists(ComponentsPath) Then
-            System.IO.Directory.CreateDirectory(ComponentsPath)
-        End If
-        myHost = New EECloud.HostAPI.CloudHost(ComponentsPath)
+        Application.Run(New EECloud.HostAPI.CloudApplicationContext)
         Console.ReadLine()
         Return 0
     End Function
