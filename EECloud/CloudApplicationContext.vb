62bc08b8a6f7ab28569d1fa88eb780e313a6df2d
Jo Jatekok <jojatekokweb@gmail.com>
2012-07-19 18:39:46 +0200

    Everything is in one directory now

--- EECloud/CloudApplicationContext.vb
+++ EECloud/CloudApplicationContext.vb
@@ -2,15 +2,15 @@
     Inherits System.Windows.Forms.ApplicationContext
 
     Public Sub New()
-        Dim PluginsPath As String = My.Application.Info.DirectoryPath & "\Plugins"
-        If Not System.IO.Directory.Exists(PluginsPath) Then
-            System.IO.Directory.CreateDirectory(PluginsPath)
-        End If
+        'Dim PluginsPath As String = My.Application.Info.DirectoryPath & "\Plugins"
+        'If Not System.IO.Directory.Exists(PluginsPath) Then
+        '    System.IO.Directory.CreateDirectory(PluginsPath)
+        'End If
 
-        Dim ComponentsPath As String = My.Application.Info.DirectoryPath & "\Components"
-        If Not System.IO.Directory.Exists(ComponentsPath) Then
-            System.IO.Directory.CreateDirectory(ComponentsPath)
-        End If
-        Dim myHost = New EECloud.HostAPI.CloudHost(ComponentsPath)
+        'Dim ComponentsPath As String = My.Application.Info.DirectoryPath & "\Components"
+        'If Not System.IO.Directory.Exists(ComponentsPath) Then
+        '    System.IO.Directory.CreateDirectory(ComponentsPath)
+        'End If
+        Dim myHost = New EECloud.HostAPI.CloudHost(My.Application.Info.DirectoryPath)
     End Sub
 End Class
