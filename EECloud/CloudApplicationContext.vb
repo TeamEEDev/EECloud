a3782a6ba27333fbc4ab4053d70ddf0ea680c41c
Yonom <sepi1376@yahoo.com>
2012-07-20 19:01:47 +0200

    No commenting out stuff

--- EECloud/CloudApplicationContext.vb
+++ EECloud/CloudApplicationContext.vb
@@ -2,15 +2,7 @@
     Inherits System.Windows.Forms.ApplicationContext
 
     Public Sub New()
-        'Dim PluginsPath As String = My.Application.Info.DirectoryPath & "\Plugins"
-        'If Not System.IO.Directory.Exists(PluginsPath) Then
-        '    System.IO.Directory.CreateDirectory(PluginsPath)
-        'End If
-
-        'Dim ComponentsPath As String = My.Application.Info.DirectoryPath & "\Components"
-        'If Not System.IO.Directory.Exists(ComponentsPath) Then
-        '    System.IO.Directory.CreateDirectory(ComponentsPath)
-        'End If
         Dim myHost = New EECloud.HostAPI.CloudHost(My.Application.Info.DirectoryPath)
+        myHost.Connections.MainConnection.LogManager.Log("dfajklsdfjk")
     End Sub
 End Class
