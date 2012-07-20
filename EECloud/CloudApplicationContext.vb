2ce7a5897c834209c681ed3db5645c7e9ccedc26
Yonom <sepi1376@yahoo.com>
2012-07-20 19:02:22 +0200

    Eh sorry

--- EECloud/CloudApplicationContext.vb
+++ EECloud/CloudApplicationContext.vb
@@ -3,6 +3,5 @@
 
     Public Sub New()
         Dim myHost = New EECloud.HostAPI.CloudHost(My.Application.Info.DirectoryPath)
-        myHost.Connections.MainConnection.LogManager.Log("dfajklsdfjk")
     End Sub
 End Class
