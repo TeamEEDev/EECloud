83fbe3d6654a518dbb62258e33150e25981a8c2f
Yonom <sepi1376@yahoo.com>
2012-07-20 18:51:53 +0200

    Fixes first blank line ;D

--- EECloud.LogManager/LogManager.vb
+++ EECloud.LogManager/LogManager.vb
@@ -19,7 +19,7 @@ Public Class LogManager
     Public Event OnInput As EventHandler Implements ILogManager.OnInput
 
     Sub New()
-        Log("") 'Init
+        Log("EECloud version: " & My.Application.Info.Version.ToString) 'Init
         Dim Worker As New Thread(
             Sub()
                 Do
