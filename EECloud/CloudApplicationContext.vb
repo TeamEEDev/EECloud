b53ceeb79525badecff4ab6211bc623633d60e58
Jojatekok <jojatekokweb@gmail.com>
2012-07-21 12:07:09 +0200

    Added title; probably full fix for log manager input bug evasion

--- EECloud/CloudApplicationContext.vb
+++ EECloud/CloudApplicationContext.vb
@@ -2,6 +2,7 @@
     Inherits System.Windows.Forms.ApplicationContext
 
     Public Sub New()
+        Console.Title = "EECloud"
         Dim myHost = New EECloud.HostAPI.CloudHost(My.Application.Info.DirectoryPath)
     End Sub
 End Class
