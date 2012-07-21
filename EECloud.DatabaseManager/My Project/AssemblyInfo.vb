a48807e0d436003e739f736732502daa4027034b
Jo Jatekok <jojatekokweb@gmail.com>
2012-07-21 16:13:10 +0200

    Added DatabaseManager

---
+++ EECloud.DatabaseManager/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EECloud.DatabaseManager")> 
+<Assembly: AssemblyDescription("Database Manager for EECloud")> 
+<Assembly: AssemblyCompany("Processor and Jojatekok")> 
+<Assembly: AssemblyProduct("EECloud.DatabaseManager")> 
+<Assembly: AssemblyCopyright("")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3327a71d-a9d5-4abc-afdc-e699f0de5086")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
