f875a27542e6421a93f6bf1381549c6c6c94732a
Yonom <sepi1376@yahoo.com>
2012-07-22 15:54:52 +0200

    Removed RegistedMessageInfo and ReciveType

--- EECloud.PluginAPI/Classes/OnMessageEventArgs.vb
+++ EECloud.PluginAPI/Classes/OnMessageEventArgs.vb
@@ -1,21 +1,19 @@
 ﻿Public Class OnMessageEventArgs
     Inherits EventArgs
-    Private m_Type As ReciveType
-    Public ReadOnly Property Type As ReciveType
+    Public ReadOnly Property Type As Type
         Get
-            Return m_Type
+            Return m_Message.GetType
         End Get
     End Property
 
-    Private m_Message As Recive.ReciveMessage
-    Public ReadOnly Property Message As Recive.ReciveMessage
+    Private m_Message As ReciveMessage
+    Public ReadOnly Property Message As ReciveMessage
         Get
             Return m_Message
         End Get
     End Property
 
-    Public Sub New(PType As ReciveType, PMessage As Recive.ReciveMessage)
-        m_Type = PType
+    Public Sub New(PMessage As ReciveMessage)
         m_Message = PMessage
     End Sub
 End Class
