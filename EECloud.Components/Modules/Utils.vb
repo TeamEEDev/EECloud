a441af3986378db24f809970942c38fd36487e13
Yonom <Yonom>
2012-07-24 23:32:46 +0200

    Plugins may not send messages!!!

--- EECloud.Components/Modules/Utils.vb
+++ EECloud.Components/Modules/Utils.vb
@@ -1,4 +1,4 @@
-﻿Public Class Config
+﻿Friend Class Config
     Public Const GameID As String = "everybody-edits-su9rn58o40itdbnw69plyw"
     Public Const NormalRoom As String = "Everybodyedits"
 End Class
