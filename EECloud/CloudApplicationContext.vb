2306d3a77537e8690d22295b5bf80cdb0da270d7
Yonom <Yonom>
2012-07-26 18:34:16 +0200

    Fixes startup errors

--- EECloud/CloudApplicationContext.vb
+++ EECloud/CloudApplicationContext.vb
@@ -9,22 +9,23 @@
             My.Settings.LicenceUsername = System.Configuration.ConfigurationManager.AppSettings("cloud.username")
             My.Settings.LicenceKey = System.Configuration.ConfigurationManager.AppSettings("cloud.key")
         ElseIf My.Settings.LicenceKey = "" Then
-
+            Console.Write("Please enter licence username: ")
+            My.Settings.LicenceUsername = Console.ReadLine()
+            Console.Write("Please enter licence key: ")
+            My.Settings.LicenceKey = Console.ReadLine()
+            My.Settings.Save()
         End If
 
-        Try
+
             Dim myBot As New Bot(AppEnvironment, My.Settings.LicenceUsername, My.Settings.LicenceKey)
             'TODO: well, ask for login defails somehow
             myBot.Logger.Log(LogPriority.Info, "Joining world...")
-            myBot.Connect("guest", "guest", "PWWOfglOCdbEI",
+        myBot.Connect("guest", "guest", "PWWOfglOCdbEI",
                 Sub(PConnection As IConnection)
-                myBot.Logger.Log(LogPriority.Info, "Successfully joined.")
-            End Sub,
+                    myBot.Logger.Log(LogPriority.Info, "Successfully joined.")
+                End Sub,
                 Sub(ex As EECloudException)
-                myBot.Logger.Log(LogPriority.Serve, "Failed to join.")
-            End Sub)
-        Catch ex As Exception
-
-        End Try
+                    myBot.Logger.Log(LogPriority.Serve, "Failed to join.")
+                End Sub)
     End Sub
 End Class
