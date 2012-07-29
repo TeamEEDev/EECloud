39fdfb066c0fea7fdc05e6ca84f732d449e8c3c7
Jo Jatekok <jojatekokweb@gmail.com>
2012-07-29 20:28:22 +0200

    Basic login form

--- EECloud/Forms/LoginForm.Designer.vb
+++ EECloud/Forms/LoginForm.Designer.vb
@@ -22,8 +22,98 @@ Partial Class LoginForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.TextBoxEmail = New System.Windows.Forms.TextBox()
+        Me.TextBoxWorldID = New System.Windows.Forms.TextBox()
+        Me.TextBoxPassword = New System.Windows.Forms.TextBox()
+        Me.ButtonJoinWorld = New System.Windows.Forms.Button()
+        Me.LabelEmail = New System.Windows.Forms.Label()
+        Me.LabelPassword = New System.Windows.Forms.Label()
+        Me.LabelWorldID = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBoxEmail
+        '
+        Me.TextBoxEmail.Location = New System.Drawing.Point(74, 12)
+        Me.TextBoxEmail.Name = "TextBoxEmail"
+        Me.TextBoxEmail.Size = New System.Drawing.Size(150, 20)
+        Me.TextBoxEmail.TabIndex = 0
+        '
+        'TextBoxWorldID
+        '
+        Me.TextBoxWorldID.Location = New System.Drawing.Point(74, 64)
+        Me.TextBoxWorldID.Name = "TextBoxWorldID"
+        Me.TextBoxWorldID.Size = New System.Drawing.Size(150, 20)
+        Me.TextBoxWorldID.TabIndex = 2
+        '
+        'TextBoxPassword
+        '
+        Me.TextBoxPassword.Location = New System.Drawing.Point(74, 38)
+        Me.TextBoxPassword.Name = "TextBoxPassword"
+        Me.TextBoxPassword.Size = New System.Drawing.Size(150, 20)
+        Me.TextBoxPassword.TabIndex = 1
+        '
+        'ButtonJoinWorld
+        '
+        Me.ButtonJoinWorld.Location = New System.Drawing.Point(15, 90)
+        Me.ButtonJoinWorld.Name = "ButtonJoinWorld"
+        Me.ButtonJoinWorld.Size = New System.Drawing.Size(209, 23)
+        Me.ButtonJoinWorld.TabIndex = 3
+        Me.ButtonJoinWorld.Text = "Join world"
+        Me.ButtonJoinWorld.UseVisualStyleBackColor = True
+        '
+        'LabelEmail
+        '
+        Me.LabelEmail.AutoSize = True
+        Me.LabelEmail.Location = New System.Drawing.Point(12, 15)
+        Me.LabelEmail.Name = "LabelEmail"
+        Me.LabelEmail.Size = New System.Drawing.Size(38, 13)
+        Me.LabelEmail.TabIndex = 0
+        Me.LabelEmail.Text = "E-mail:"
+        '
+        'LabelPassword
+        '
+        Me.LabelPassword.AutoSize = True
+        Me.LabelPassword.Location = New System.Drawing.Point(12, 41)
+        Me.LabelPassword.Name = "LabelPassword"
+        Me.LabelPassword.Size = New System.Drawing.Size(56, 13)
+        Me.LabelPassword.TabIndex = 0
+        Me.LabelPassword.Text = "Password:"
+        '
+        'LabelWorldID
+        '
+        Me.LabelWorldID.AutoSize = True
+        Me.LabelWorldID.Location = New System.Drawing.Point(12, 67)
+        Me.LabelWorldID.Name = "LabelWorldID"
+        Me.LabelWorldID.Size = New System.Drawing.Size(52, 13)
+        Me.LabelWorldID.TabIndex = 0
+        Me.LabelWorldID.Text = "World ID:"
+        '
+        'LoginForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "LoginForm"
+        Me.ClientSize = New System.Drawing.Size(236, 125)
+        Me.Controls.Add(Me.LabelWorldID)
+        Me.Controls.Add(Me.LabelPassword)
+        Me.Controls.Add(Me.LabelEmail)
+        Me.Controls.Add(Me.ButtonJoinWorld)
+        Me.Controls.Add(Me.TextBoxPassword)
+        Me.Controls.Add(Me.TextBoxWorldID)
+        Me.Controls.Add(Me.TextBoxEmail)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "LoginForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Join world"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents TextBoxEmail As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxWorldID As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxPassword As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonJoinWorld As System.Windows.Forms.Button
+    Friend WithEvents LabelEmail As System.Windows.Forms.Label
+    Friend WithEvents LabelPassword As System.Windows.Forms.Label
+    Friend WithEvents LabelWorldID As System.Windows.Forms.Label
 End Class
--- EECloud/Forms/LoginForm.vb
+++ EECloud/Forms/LoginForm.vb
@@ -1,3 +1,34 @@
 ﻿Public Class LoginForm
+    Public Sub New()
+        Me.Icon = My.Resources.Icon
+        InitializeComponent()
 
+        TextBoxEmail.Text = My.Settings.LoginEmail
+        TextBoxPassword.Text = My.Settings.LoginPassword
+        TextBoxWorldID.Text = My.Settings.LoginWorldID
+    End Sub
+
+    Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Shown
+        If TextBoxEmail.Text <> "" AndAlso TextBoxPassword.Text <> "" AndAlso TextBoxWorldID.Text <> "" Then
+            ButtonJoinWorld.Focus()
+        End If
+    End Sub
+
+    Private Sub TextBoxes_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles TextBoxEmail.KeyDown, TextBoxPassword.KeyDown, TextBoxWorldID.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            ButtonJoinWorld_Click(Nothing, Nothing)
+        End If
+    End Sub
+
+    Private Sub ButtonJoinWorld_Click(sender As Object, e As EventArgs) Handles ButtonJoinWorld.Click
+        If TextBoxEmail.Text <> "" AndAlso TextBoxPassword.Text <> "" AndAlso TextBoxWorldID.Text <> "" Then
+            My.Settings.LoginEmail = TextBoxEmail.Text
+            My.Settings.LoginPassword = TextBoxPassword.Text
+            My.Settings.LoginWorldID = TextBoxWorldID.Text
+            My.Settings.Save()
+            Me.Close()
+        Else
+            MsgBox("You didn't enter some information", MsgBoxStyle.Critical, "Error")
+        End If
+    End Sub
 End Class
