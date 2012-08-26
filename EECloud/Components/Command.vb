7ccb67480fefb0da24c34e79483827e1854035c4
Yonom <sepi1376@yahoo.com>
2012-08-26 14:36:20 +0200

    Implemented Command

---
+++ EECloud/Components/Command.vb
@@ -0,0 +1,28 @@
+﻿Imports System.IO
+Imports System.IO.IsolatedStorage
+
+Public Class Command
+    Implements ICommand
+
+    Private myArgs As String()
+    Public ReadOnly Property Args As String() Implements ICommand.Args
+        Get
+            Return myArgs
+        End Get
+    End Property
+
+    Private mySender As Player
+    Public ReadOnly Property Sender As Player Implements ICommand.Sender
+        Get
+            Return mySender
+        End Get
+    End Property
+
+
+    Private myType As String
+    Public ReadOnly Property Type As String Implements ICommand.Type
+        Get
+            Return myType
+        End Get
+    End Property
+End Class
