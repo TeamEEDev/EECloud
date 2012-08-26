5731ac820a4e497f8a91fde6fa7212f00bae5596
Yonom <sepi1376@yahoo.com>
2012-08-26 16:16:31 +0200

    awesome stuff

--- EECloud.API/Components/Command.vb
+++ EECloud.API/Components/Command.vb
@@ -2,24 +2,28 @@
 Imports System.IO.IsolatedStorage
 
 Public Class Command
-    Implements ICommand
+    Friend Sub New(sender As Player, type As String, args As String())
+        Me.mySender = sender
+        Me.myType = type
+        Me.myArgs = args
+    End Sub
 
     Private myArgs As String()
-    Public ReadOnly Property Args As String() Implements ICommand.Args
+    Public ReadOnly Property Args As String()
         Get
             Return myArgs
         End Get
     End Property
 
     Private mySender As Player
-    Public ReadOnly Property Sender As Player Implements ICommand.Sender
+    Public ReadOnly Property Sender As Player
         Get
             Return mySender
         End Get
     End Property
 
     Private myType As String
-    Public ReadOnly Property Type As String Implements ICommand.Type
+    Public ReadOnly Property Type As String
         Get
             Return myType
         End Get
--- EECloud.API/Interfaces/ICommand.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Interface ICommand
-    ReadOnly Property Type As String
-    ReadOnly Property Args As String()
-    ReadOnly Property Sender As Player
-End Interface
