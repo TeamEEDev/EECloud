d87c890874bc6e894379e92d317a08daf6afe665
Jojatekok <jojatekokweb@gmail.com>
2012-08-27 14:38:48 +0200

    Prepared a project for Yo!Scroll

---
+++ Yo!Scroll Plugin/Yo!Scroll Plugin.vb
@@ -0,0 +1,29 @@
+﻿<Plugin(Authors:={"Processor", "Jojatekok"},
+        Category:=PluginCategory.LevelBot,
+        Description:="Yo!Scroll level bot",
+        StartupPriority:=PluginStartupPriority.Normal,
+        Version:="1.0.0.0")>
+Public Class YoScroll_Plugin
+    Inherits Plugin(Of YoScrollPlayer)
+    Private WithEvents Connection As Connection(Of YoScrollPlayer)
+    Private Chatter As IChatter
+
+    Protected Overrides Sub OnEnable()
+
+    End Sub
+
+    Protected Overrides Sub OnDisable()
+
+    End Sub
+
+    Protected Overrides Sub OnConnect(mainConnection As Connection(Of YoScrollPlayer))
+        Me.Connection = mainConnection
+        Chatter = Connection.GetChatter("Yo!Scroll Plugin")
+        Chatter.Chat("Welcome to Yo!Scroll!")
+    End Sub
+End Class
+
+Public Class YoScrollPlayer
+    Inherits Player
+
+End Class
