3cb287b7104275323de04c20ca957c0eb1bb36bf
Yonom <Yonom>
2012-08-27 16:43:11 +0200

    do 2 examples, then i'll do it all

---
+++ EECloud.API/Recive/ReciveMessage.vb
@@ -0,0 +1,9 @@
+﻿Public MustInherit Class ReciveMessage
+    Inherits EventArgs
+    <System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Advanced)>
+    Public ReadOnly PlayerIOMessage As PlayerIOClient.Message
+
+    Friend Sub New(message As PlayerIOClient.Message)
+        PlayerIOMessage = message
+    End Sub
+End Class
