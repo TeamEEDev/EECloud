cbd00890aad75814ee936c49e8664a9973893dce
Jo Jatekok <jojatekokweb@gmail.com>
2012-08-27 19:04:43 +0200

    Chat queue v0.0000000000000000001

--- EECloud/Components/Chatter.vb
+++ EECloud/Components/Chatter.vb
@@ -4,24 +4,35 @@
     Dim myConnection As Connection(Of Player)
     Dim prefix As String
 
+    Dim chatQueue As New Queue(Of String)
+
     Friend Sub New(connection As Connection(Of Player), name As String)
         myConnection = connection
         prefix = "<" & name & "> "
+
+        Dim SendThread As New Timers.Timer With {.Enabled = True, .Interval = 700}
+        AddHandler SendThread.Elapsed, Sub()
+                                           If chatQueue.Count > 0 Then
+                                               myConnection.Send(New Say_SendMessage(chatQueue.Peek()))
+                                               chatQueue.Dequeue()
+                                           End If
+                                       End Sub
     End Sub
 
     Public Sub Chat(msg As String) Implements IChatter.Chat
-        myConnection.Send(New Say_SendMessage(prefix & msg))
+        chatQueue.Enqueue(prefix & msg)
     End Sub
 
     Public Sub Kick(user As Player, msg As String) Implements IChatter.Kick
-        myConnection.Send(New Say_SendMessage("/kick " & user.Username & " " & prefix & msg))
+        chatQueue.Enqueue("/kick " & user.Username & " " & prefix & msg)
+        'myConnection.Send(New Say_SendMessage("/kick " & user.Username & " " & prefix & msg))
     End Sub
 
     Public Sub Loadlevel() Implements IChatter.Loadlevel
-        myConnection.Send(New Say_SendMessage("/loadlevel"))
+        chatQueue.Enqueue("/loadlevel")
     End Sub
 
     Public Sub Reset() Implements IChatter.Reset
-        myConnection.Send(New Say_SendMessage("/reset"))
+        chatQueue.Enqueue("/reset")
     End Sub
 End Class
