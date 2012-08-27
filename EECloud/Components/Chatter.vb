f570cd5a1aa525d2b1ec9ebb63e6229b1987ca89
Yonom <Yonom>
2012-08-27 19:13:52 +0200

    Better Chatter version

--- EECloud/Components/Chatter.vb
+++ EECloud/Components/Chatter.vb
@@ -4,35 +4,45 @@
     Dim myConnection As Connection(Of Player)
     Dim prefix As String
 
-    Dim chatQueue As New Queue(Of String)
+    Dim chatQueue As New Queue(Of Say_SendMessage)
+    Dim WithEvents SendTimer As New Timers.Timer With {.Enabled = True, .AutoReset = True, .Interval = 700}
+
 
     Friend Sub New(connection As Connection(Of Player), name As String)
         myConnection = connection
         prefix = "<" & name & "> "
+    End Sub
 
-        Dim SendThread As New Timers.Timer With {.Enabled = True, .Interval = 700}
-        AddHandler SendThread.Elapsed, Sub()
-                                           If chatQueue.Count > 0 Then
-                                               myConnection.Send(New Say_SendMessage(chatQueue.Peek()))
-                                               chatQueue.Dequeue()
-                                           End If
-                                       End Sub
+    Private Sub SendChat(msg As String)
+        chatQueue.Enqueue(New Say_SendMessage(msg))
+        If Not SendTimer.Enabled Then
+            SendTimer.Enabled = True
+        End If
     End Sub
 
     Public Sub Chat(msg As String) Implements IChatter.Chat
-        chatQueue.Enqueue(prefix & msg)
+        SendChat(prefix & msg)
     End Sub
 
     Public Sub Kick(user As Player, msg As String) Implements IChatter.Kick
-        chatQueue.Enqueue("/kick " & user.Username & " " & prefix & msg)
-        'myConnection.Send(New Say_SendMessage("/kick " & user.Username & " " & prefix & msg))
+        SendChat("/kick " & user.Username & " " & prefix & msg)
     End Sub
 
     Public Sub Loadlevel() Implements IChatter.Loadlevel
-        chatQueue.Enqueue("/loadlevel")
+        SendChat("/loadlevel")
     End Sub
 
     Public Sub Reset() Implements IChatter.Reset
-        chatQueue.Enqueue("/reset")
+        SendChat("/reset")
+    End Sub
+
+    Private Sub SendTimer_Elapsed(sender As Object, e As Timers.ElapsedEventArgs) Handles SendTimer.Elapsed
+        SyncLock chatQueue 'Super important to make code thread safe
+            If chatQueue.Count > 0 Then
+                myConnection.Send(chatQueue.Dequeue)
+            Else
+                SendTimer.Enabled = False
+            End If
+        End SyncLock
     End Sub
 End Class
