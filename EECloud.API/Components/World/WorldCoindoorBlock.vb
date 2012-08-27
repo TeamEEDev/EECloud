839efca9efaa59383a1d053bc60a31b64de14a93
Yonom <Yonom>
2012-08-27 16:15:18 +0200

    huh?

---
+++ EECloud.API/Components/World/WorldCoindoorBlock.vb
@@ -0,0 +1,16 @@
+﻿Public Class WorldCoindoorBlock
+    Inherits WorldBlock
+
+    Friend Sub New(layer As Layer, x As Integer, y As Integer, block As CoindoorBlockType, coinsToCollect As Integer)
+        MyBase.New(layer, x, y, CType(block, BlockType))
+
+        Me.myCoinsToCollect = coinsToCollect
+    End Sub
+
+    Private myCoinsToCollect As Integer
+    Public ReadOnly Property CoinsToCollect As Integer
+        Get
+            Return myCoinsToCollect
+        End Get
+    End Property
+End Class
