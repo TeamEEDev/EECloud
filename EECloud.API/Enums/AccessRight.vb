74ee3046a00f6115db4b2930aa06db5d6756c30e
Yonom <Yonom>
2012-10-07 11:42:14 +0200

    Documentation

--- EECloud.API/Enums/AccessRight.vb
+++ EECloud.API/Enums/AccessRight.vb
@@ -1,5 +1,21 @@
-﻿Public Enum AccessRight
+﻿''' <summary>
+''' Represents the rights of the bot connection in the world.
+''' </summary>
+''' <remarks></remarks>
+Public Enum AccessRight
+    ''' <summary>
+    ''' Represemts the state where the bot does not have any rights in the world.
+    ''' </summary>
+    ''' <remarks></remarks>
     None = 0
+    ''' <summary>
+    ''' Represents the state where the bot has edit rights in the world.
+    ''' </summary>
+    ''' <remarks></remarks>
     Edit = 1
+    ''' <summary>
+    ''' Represents the state where bot has command access in the world.
+    ''' </summary>
+    ''' <remarks></remarks>
     Full = 2
 End Enum
