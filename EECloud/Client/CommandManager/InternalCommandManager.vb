d1ceabbf8e1f031e35839b289526de6d8af0c42a
Yonom <sepi1376@yahoo.com>
2012-10-14 13:24:50 +0200

    Fixes

--- EECloud/Client/CommandManager/InternalCommandManager.vb
+++ EECloud/Client/CommandManager/InternalCommandManager.vb
@@ -5,7 +5,7 @@
 #End Region
 
 #Region "Events"
-    Friend Event OnCommand(msg As String, user As Integer, e As CommandEventArgs)
+    Friend Event OnCommand(msg As String, user As Integer, rights As Group, e As CommandEventArgs)
 #End Region
 
 #Region "Methods"
@@ -14,18 +14,18 @@
         myClient = client
         AddHandler myClient.Connection.ReceiveSay, AddressOf myConnection_OnReceiveSay
         AddHandler Cloud.Logger.OnInput,
-            Sub(sender As Object, e As EventArgs) HandleMessage(Cloud.Logger.Input, -1)
+            Sub(sender As Object, e As EventArgs) HandleMessage(Cloud.Logger.Input, -1, Group.Host)
     End Sub
 
     Private Sub myConnection_OnReceiveSay(sender As Object, e As SayReceiveMessage)
         If e.Text.StartsWith("!", StringComparison.Ordinal) Then
-            HandleMessage(e.Text.Substring(1), e.UserID)
+            HandleMessage(e.Text.Substring(1), e.UserID, Group.Limited)
         End If
     End Sub
 
-    Friend Sub HandleMessage(msg As String, user As Integer)
+    Friend Sub HandleMessage(msg As String, user As Integer, rights As Group)
         Dim eventArgs As New CommandEventArgs
-        RaiseEvent OnCommand(msg, user, eventArgs)
+        RaiseEvent OnCommand(msg, user, rights, eventArgs)
         If eventArgs.Handled = False Then
             If myClient.InternalPlayerManager.Players.ContainsKey(user) Then
                 Dim sender As IPlayer = myClient.InternalPlayerManager.Players(user)
