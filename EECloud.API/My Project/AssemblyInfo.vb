d3adc778eef090af528af8d752736ac8757536be
Yonom <sepi1376@yahoo.com>
2012-11-13 20:55:08 +0100

    Fixes. Fixes everywhere

--- EECloud.API/My Project/AssemblyInfo.vb
+++ EECloud.API/My Project/AssemblyInfo.vb
@@ -1,7 +1,6 @@
 ﻿Imports System.Reflection
 Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
-Imports System.Resources
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
@@ -11,29 +10,9 @@ Imports System.Resources
 
 <Assembly: AssemblyTitle("EECloud.API Beta")> 
 <Assembly: AssemblyDescription("EECloud API")>
-<Assembly: AssemblyCompany("EEDev")> 
-<Assembly: AssemblyProduct("EECloud")>
-<Assembly: AssemblyCopyright("")>
-<Assembly: AssemblyTrademark("")>
 <Assembly: InternalsVisibleTo("EECloud")> 
 
 <Assembly: ComVisible(False)>
 
     'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("a30271ce-0b1e-47ce-b43e-ee3ef25f12c5")>
-
-    ' Version information for an assembly consists of the following four values:
-    '
-    '      Major Version
-    '      Minor Version 
-    '      Build Number
-    '      Revision
-    '
-    ' You can specify all the values or you can default the Build and Revision Numbers 
-    ' by using the '*' as shown below:
-    ' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("0.5.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
-
-<Assembly: NeutralResourcesLanguage("")>
--- EECloud/My Project/AssemblyInfo.vb
+++ EECloud/My Project/AssemblyInfo.vb
@@ -1,6 +1,5 @@
 ﻿Imports System.Reflection
 Imports System.Runtime.InteropServices
-Imports System.Resources
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
@@ -10,28 +9,8 @@ Imports System.Resources
 
 <Assembly: AssemblyTitle("EECloud Beta")> 
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("EEDev")>
-<Assembly: AssemblyProduct("EECloud")>
-<Assembly: AssemblyCopyright("")>
-<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
     'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("f5238632-035d-49d6-b9a7-d8af5300a6bb")>
-
-    ' Version information for an assembly consists of the following four values:
-    '
-    '      Major Version
-    '      Minor Version 
-    '      Build Number
-    '      Revision
-    '
-    ' You can specify all the values or you can default the Build and Revision Numbers 
-    ' by using the '*' as shown below:
-    ' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("0.5.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
-
-<Assembly: NeutralResourcesLanguage("en")>
