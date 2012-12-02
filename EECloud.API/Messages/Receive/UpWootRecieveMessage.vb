2ceac91a904e88c13ee0da91fb474ee66be7557c
Yonom <sepi1376@yahoo.com>
2012-12-02 15:13:11 +0100

    Added Woot recievemessage support

---
+++ EECloud.API/Messages/Receive/UpWootRecieveMessage.vb
@@ -0,0 +1,12 @@
+﻿Imports PlayerIOClient
+
+Public Class UpWootRecieveMessage
+    Inherits ReceiveMessage
+    Public ReadOnly UserID As Integer
+    '0
+
+    Friend Sub New(message As Message)
+        MyBase.New(message)
+        UserID = message.GetInteger(0)
+    End Sub
+End Class
