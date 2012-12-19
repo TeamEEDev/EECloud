6cdbdc7e32f08f5a866bb55bad65edf8f45d1764
Yonom <sepi1376@yahoo.com>
2012-12-19 21:05:37 +0100

    Added Rotation send and upload messages

---
+++ EECloud.API/Messages/Send/RotatablePlaceSendMessage.vb
@@ -0,0 +1,22 @@
+﻿Imports PlayerIOClient
+
+Public Class RotatablePlaceSendMessage
+    Inherits BlockPlaceSendMessage
+    Public ReadOnly Rotation As Integer
+
+    Public Sub New(layer As Layer, x As Integer, y As Integer, block As RotatableBlock, coinsToCollect As Integer)
+        MyBase.New(layer, x, y, CType(block, Block))
+
+        Me.Rotation = coinsToCollect
+    End Sub
+
+    Friend Overrides Function GetMessage(ByVal game As IGame) As Message
+        If IsCoinDoor(Block) Then
+            Dim message As Message = MyBase.GetMessage(game)
+            message.Add(Rotation)
+            Return message
+        Else
+            Return MyBase.GetMessage(game)
+        End If
+    End Function
+End Class
