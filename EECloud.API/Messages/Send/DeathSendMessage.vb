ede345a3f69974b3ddf9a7923de41da888671e57
Yonom <sepi1376@yahoo.com>
2012-12-20 15:01:03 +0100

    Added Checkpoints and Death send messages

---
+++ EECloud.API/Messages/Send/DeathSendMessage.vb
@@ -0,0 +1,10 @@
+﻿Imports PlayerIOClient
+
+Public Class DeathSendMessage
+    Inherits SendMessage
+    'No arguments
+
+    Friend Overrides Function GetMessage(ByVal game As IGame) As Message
+        Return Message.Create("death")
+    End Function
+End Class
