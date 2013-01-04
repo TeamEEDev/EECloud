0760ad0087dd198d70fd90099096143885cfee05
Yonom <sepehr1376@gmail.com>
2013-01-04 20:54:47 +0100

    Updated version

--- EECloud/SharedAssemblyInfo.vb
+++ EECloud/SharedAssemblyInfo.vb
@@ -21,4 +21,4 @@
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
-<Assembly: AssemblyVersion("0.9.6.0")> 
+<Assembly: AssemblyVersion("0.9.7.0")> 
