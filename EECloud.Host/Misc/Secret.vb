2dac4c3918218d0e038c85f5f4fa3b771ed5d618
Yonom <sepi1376@yahoo.com>
2013-01-10 21:32:40 +0100

    updated mysql password because jojatekok changed it -.-

--- EECloud.Host/Misc/Secret.vb
+++ EECloud.Host/Misc/Secret.vb
@@ -7,7 +7,7 @@
 #Region "Really?"
 
 #Region "Okay..."
-    Friend Const ConnStr As String = "server=16392fcf-3bd4-4a1d-aba5-a0cd00c9dcf0.mysql.sequelizer.com;database=db16392fcf3bd44a1daba5a0cd00c9dcf0;uid=lynamplbptkrailf;pwd=uJGb5bQPBMmDsCwweiADhHhZvRvnURUnXJY2xyvypYGZx6TgybJY36eTBQMmNC3T"
+    Friend Const ConnStr As String = "server=16392fcf-3bd4-4a1d-aba5-a0cd00c9dcf0.mysql.sequelizer.com;database=db16392fcf3bd44a1daba5a0cd00c9dcf0;uid=lynamplbptkrailf;pwd=pn4RKMdLWN8F8fBebHzefe4Q5msekLnCDa5d37pQvAMAmNygNBkRiPrjsmVxu8Aj"
 #End Region
 
 #End Region
