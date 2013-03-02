15b579001b7c6006749cd6be2514dfbb5d58b859
Jojatekok <jojatekokweb@gmail.com>
2013-03-02 08:16:31 +0100

    Designer error fixed

--- EECloud.Launcher.WinForms/Form1.Designer.cs
+++ EECloud.Launcher.WinForms/Form1.Designer.cs
@@ -308,7 +308,6 @@
             this.Name = "Form1";
             this.Text = "EECloud Launcher";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
