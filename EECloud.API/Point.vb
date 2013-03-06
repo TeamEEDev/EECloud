5b33fe99cee5ebc7de0c7894b36c4cff8abd28b3
Jojatekok <jojatekokweb@gmail.com>
2013-03-06 19:03:24 +0100

    A custom class is always better

---
+++ EECloud.API/Point.vb
@@ -0,0 +1,9 @@
+﻿Public Class Point
+    Public Property X As Integer
+    Public Property Y As Integer
+
+    Public Sub New(x As Integer, y As Integer)
+        Me.X = x
+        Me.Y = y
+    End Sub
+End Class
