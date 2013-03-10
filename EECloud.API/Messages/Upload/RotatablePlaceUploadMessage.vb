3e216d897bc16aa4726cc2166f1ee0b81fe58743
Jojatekok <jojatekokweb@gmail.com>
2013-03-10 10:12:01 +0100

    Fixed a bug

--- EECloud.API/Messages/Upload/RotatablePlaceUploadMessage.vb
+++ EECloud.API/Messages/Upload/RotatablePlaceUploadMessage.vb
@@ -2,9 +2,9 @@
 
 Public Class RotatablePlaceUploadMessage
     Inherits BlockPlaceUploadMessage
-    Public ReadOnly Rotation As Integer
+    Public ReadOnly Rotation As BlockRotation
 
-    Public Sub New(layer As Layer, x As Integer, y As Integer, block As RotatableBlock, rotation As Integer)
+    Public Sub New(layer As Layer, x As Integer, y As Integer, block As RotatableBlock, rotation As BlockRotation)
         MyBase.New(layer, x, y, DirectCast(block, Block))
 
         Me.Rotation = rotation
@@ -13,7 +13,7 @@ Public Class RotatablePlaceUploadMessage
     Friend Overrides Function GetMessage(game As IGame) As Message
         If IsRotatable(Block) Then
             Dim message As Message = MyBase.GetMessage(game)
-            message.Add(Rotation)
+            message.Add(CInt(Rotation))
             Return message
         Else
             Return MyBase.GetMessage(game)
