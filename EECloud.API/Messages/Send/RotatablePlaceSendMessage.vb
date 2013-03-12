ca1d5ec78d16e3d3e03cb11b7d93a22aecea5eba
Yonom <sepi1376@yahoo.com>
2013-03-12 22:27:54 +0100

    SpikeRotation is just to be specified instead of the integer parameter
    Future blocks may not follow the same directions as the Spikes (ex.
    Skifi pack uses NorthEast, SouthEast etc.)

--- EECloud.API/Messages/Send/RotatablePlaceSendMessage.vb
+++ EECloud.API/Messages/Send/RotatablePlaceSendMessage.vb
@@ -2,9 +2,9 @@
 
 Public Class RotatablePlaceSendMessage
     Inherits BlockPlaceSendMessage
-    Public ReadOnly Rotation As BlockRotation
+    Public ReadOnly Rotation As Integer
 
-    Public Sub New(layer As Layer, x As Integer, y As Integer, block As RotatableBlock, rotation As BlockRotation)
+    Public Sub New(layer As Layer, x As Integer, y As Integer, block As RotatableBlock, rotation As Integer)
         MyBase.New(layer, x, y, DirectCast(block, Block))
 
         Me.Rotation = rotation
@@ -13,7 +13,7 @@ Public Class RotatablePlaceSendMessage
     Friend Overrides Function GetMessage(game As IGame) As Message
         If IsRotatable(Block) Then
             Dim message As Message = MyBase.GetMessage(game)
-            message.Add(CInt(Rotation))
+            message.Add(Rotation)
             Return message
         Else
             Return MyBase.GetMessage(game)
