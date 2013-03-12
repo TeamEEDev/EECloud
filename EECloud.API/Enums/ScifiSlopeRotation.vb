05b08ed8ff22268926b3f71384bd3b4840f176e7
Yonom <sepehr1376@gmail.com>
2013-03-12 22:37:14 +0100

    Added directions

---
+++ EECloud.API/Enums/ScifiSlopeRotation.vb
@@ -0,0 +1,4 @@
+﻿Public Enum ScifiStraightRotation
+    Vertical = 0
+    Horizontal = 1
+End Enum
---
+++ EECloud.API/Enums/ScifiStraightRotation.vb
@@ -0,0 +1,6 @@
+﻿Public Enum ScifiSlopeRotation
+    SouthWest = 0
+    SouthEast = 1
+    NorthEast = 2
+    NorthWest = 3
+End Enum
