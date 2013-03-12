f224c3de241c60780d33b1ef01a0bb2776b8d572
Yonom <sepehr1376@gmail.com>
2013-03-12 22:24:06 +0100

    Revert "No it's not"
    This reverts commit 3426c2bd50439ae435fd6c78a825e6d0684eaf3b.

--- EECloud.API/Messages/Receive/TeleportReceiveMessage.vb
+++ EECloud.API/Messages/Receive/TeleportReceiveMessage.vb
@@ -4,7 +4,7 @@ Public NotInheritable Class TeleportReceiveMessage
     Inherits ReceiveMessage
     Public ReadOnly ResetCoins As Boolean
     '0
-    Public ReadOnly Coordinates As New Dictionary(Of Integer, KeyValuePair(Of Integer, Integer))
+    Public ReadOnly Coordinates As New Dictionary(Of Integer, Point)
 
     Friend Sub New(message As Message)
         MyBase.New(message)
@@ -12,7 +12,7 @@ Public NotInheritable Class TeleportReceiveMessage
         ResetCoins = message.GetBoolean(0)
 
         For i As UInteger = 1 To CUInt(message.Count - 1) Step 3
-            Coordinates.Add(message.GetInteger(i), New KeyValuePair(Of Integer, Integer)(message.GetInteger(CUInt(i + 1)), message.GetInteger(CUInt(i + 2))))
+            Coordinates.Add(message.GetInteger(i), New Point(message.GetInteger(CUInt(i + 1)), message.GetInteger(CUInt(i + 2))))
         Next
     End Sub
 End Class
