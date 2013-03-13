b63f3cf9de38e9e8438a15ed715b92d1205235b6
Jo Jatekok <jojatekokweb@gmail.com>
2013-03-13 14:17:18 +0100

    Fixed Your error haha ;p

--- EECloud.API/Messages/Upload/PortalPlaceUploadMessage.vb
+++ EECloud.API/Messages/Upload/PortalPlaceUploadMessage.vb
@@ -18,7 +18,7 @@ Public NotInheritable Class PortalPlaceUploadMessage
     Friend Overrides Function GetMessage(game As IGame) As Message
         If IsPortal(Block) Then
             Dim message As Message = MyBase.GetMessage(game)
-            message.Add(PortalRotation,
+            message.Add(CInt(PortalRotation),
                         PortalID,
                         PortalTarget)
             Return message
