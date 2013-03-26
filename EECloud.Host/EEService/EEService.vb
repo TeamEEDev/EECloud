d8e5894c1fdace4d270658f5c1cd4c79220d0d76
Jojatekok <jojatekokweb@gmail.com>
2013-03-26 15:36:03 +0100

    Added custom null checking method for strings

--- EECloud.Host/EEService/EEService.vb
+++ EECloud.Host/EEService/EEService.vb
@@ -8,7 +8,7 @@
     End Property
 
     Friend Function GetSetting(key As String) As String Implements IEEService.GetSetting
-        If String.IsNullOrEmpty(key) Then
+        If StringIsNullOrEmpty(key) Then
             Throw New ArgumentNullException("key")
         End If
 
@@ -30,7 +30,7 @@
         End If
 
         For i = 0 To keyList.Length - 1
-            If String.IsNullOrEmpty(keyList(i)) Then
+            If StringIsNullOrEmpty(keyList(i)) Then
                 Throw New ArgumentNullException("keyList", "'KeyList' mustn't contain empty or null values.")
             End If
         Next
@@ -64,10 +64,10 @@
     End Function
 
     Friend Sub SetSetting(key As String, value As String) Implements IEEService.SetSetting
-        If String.IsNullOrEmpty(key) Then
+        If StringIsNullOrEmpty(key) Then
             Throw New ArgumentNullException("key")
         End If
-        If String.IsNullOrEmpty(value) Then
+        If StringIsNullOrEmpty(value) Then
             Throw New ArgumentNullException("value")
         End If
 
@@ -90,7 +90,7 @@
         End If
 
         For i = 0 To keyValuePairs.Length - 1
-            If String.IsNullOrEmpty(keyValuePairs(i).Key) OrElse String.IsNullOrEmpty(keyValuePairs(i).Value) Then
+            If StringIsNullOrEmpty(keyValuePairs(i).Key) OrElse StringIsNullOrEmpty(keyValuePairs(i).Value) Then
                 Throw New ArgumentNullException("keyValuePairs", "'KeyValuePairs' mustn't contain empty or null values.")
             End If
         Next
@@ -111,7 +111,7 @@
     End Sub
 
     Friend Function GetPlayerData(username As String) As UserData Implements IEEService.GetPlayerData
-        If String.IsNullOrEmpty(username) Then
+        If StringIsNullOrEmpty(username) Then
             Throw New ArgumentNullException("username")
         End If
 
@@ -207,7 +207,7 @@
     Private Shared ReadOnly myAcceptedGroupIDs() As Short = {400, 300, 100, 0, -100, -200}
 
     Friend Sub SetPlayerDataGroupID(username As String, groupID As Short) Implements IEEService.SetPlayerDataGroupID
-        If String.IsNullOrEmpty(username) Then
+        If StringIsNullOrEmpty(username) Then
             Throw New ArgumentNullException("username")
         End If
         If Not myAcceptedGroupIDs.Contains(groupID) Then
@@ -228,7 +228,7 @@
     End Sub
 
     Friend Sub SetPlayerDataYoScrollWins(username As String, yoScrollWins As UShort) Implements IEEService.SetPlayerDataYoScrollWins
-        If String.IsNullOrEmpty(username) Then
+        If StringIsNullOrEmpty(username) Then
             Throw New ArgumentNullException("username")
         End If
 
@@ -246,7 +246,7 @@
     End Sub
 
     Friend Sub SetPlayerDataFTBreakerWins(username As String, ftBreakerWins As UShort) Implements IEEService.SetPlayerDataFTBreakerWins
-        If String.IsNullOrEmpty(username) Then
+        If StringIsNullOrEmpty(username) Then
             Throw New ArgumentNullException("username")
         End If
 
@@ -264,7 +264,7 @@
     End Sub
 
     Friend Function GetFacts(factGroup As String) As String() Implements IEEService.GetFacts
-        If String.IsNullOrEmpty(factGroup) Then
+        If StringIsNullOrEmpty(factGroup) Then
             Throw New ArgumentNullException("factGroup")
         End If
 
@@ -287,7 +287,7 @@
     End Function
 
     Friend Sub RemoveFact(factID As String) Implements IEEService.RemoveFact
-        If String.IsNullOrEmpty(factID) Then
+        If StringIsNullOrEmpty(factID) Then
             Throw New ArgumentNullException("factID")
         End If
 
@@ -307,10 +307,10 @@
     End Sub
 
     Friend Sub SetFact(factID As String, factGroup As String) Implements IEEService.SetFact
-        If String.IsNullOrEmpty(factID) Then
+        If StringIsNullOrEmpty(factID) Then
             Throw New ArgumentNullException("factID")
         End If
-        If String.IsNullOrEmpty(factGroup) Then
+        If StringIsNullOrEmpty(factGroup) Then
             Throw New ArgumentNullException("factGroup")
         End If
 
