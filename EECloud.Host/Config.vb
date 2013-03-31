b1120e2b3c285b76b9bcb4b40c1d8dba87cacddc
Jojatekok <jojatekokweb@gmail.com>
2013-03-31 13:58:27 +0200

    Added a test

--- EECloud.Host/Config.vb
+++ EECloud.Host/Config.vb
@@ -11,6 +11,30 @@ Friend Module Config
 
     'Custom null checking method for strings
     Friend Function StringIsNullOrEmpty(value As String) As Boolean
+        value = "                                           " & vbLf & "abc978d   4 " & vbCr & "6" & vbCrLf & ":)"
+
+        Dim watch1 As New Stopwatch
+        watch1.Start()
+        For i = 0 To 1000000
+            If value = Nothing OrElse value.Length = 0 OrElse myWhitespacesRegex.Replace(value, String.Empty).Length = 0 Then
+
+            End If
+        Next
+        watch1.Stop()
+
+        Dim watch2 As New Stopwatch
+        watch2.Start()
+        For i = 0 To 1000000
+            If String.IsNullOrWhiteSpace(value) Then
+
+            End If
+        Next
+        watch2.Stop()
+
+        MsgBox("Official method: " & watch2.ElapsedMilliseconds & "ms" & Environment.NewLine &
+               "Custom method: " & watch1.ElapsedMilliseconds & "ms")
+        Environment.Exit(0)
+
         If value = Nothing OrElse value.Length = 0 OrElse myWhitespacesRegex.Replace(value, String.Empty).Length = 0 Then
             Return True
         End If
