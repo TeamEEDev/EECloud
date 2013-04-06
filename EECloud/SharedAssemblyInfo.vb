fa518f0b81bb9e2f7aec0f4c220d11135b97c123
Jojatekok <jojatekokweb@gmail.com>
2013-04-06 12:32:24 +0200

    Don't build EECloud.Setup in debug mode! :)

--- EECloud/SharedAssemblyInfo.vb
+++ EECloud/SharedAssemblyInfo.vb
@@ -19,4 +19,4 @@
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
-<Assembly: AssemblyVersion("0.9.29.0")> 
+<Assembly: AssemblyVersion("0.9.29")> 
