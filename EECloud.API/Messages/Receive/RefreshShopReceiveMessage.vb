c8a5034089f31b60516fa882544cc6504b826a5d
Jojatekok <jojatekokweb@gmail.com>
2013-05-17 22:50:52 +0200

    Whoops, typo ;p

--- EECloud.API/Messages/Receive/RefreshShopReceiveMessage.vb
+++ EECloud.API/Messages/Receive/RefreshShopReceiveMessage.vb
@@ -3,7 +3,7 @@
 Public NotInheritable Class RefreshShopReceiveMessage
     Inherits ReceiveMessage
 
-    'No arguments; this is just the request to refresh the shop on the client-side.
+    'No arguments; this is just a request to refresh the shop on the client-side.
 
     Friend Sub New(message As Message)
         MyBase.New(message)
