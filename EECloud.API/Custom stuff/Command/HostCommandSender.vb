daf6c3bd6ffe75a3769349c91d7786054ca8a21a
Yonom <sepi1376@yahoo.com>
2013-07-31 19:16:08 +0200

    Revert "Revert "Revert "Last commit!"""
    This reverts commit 84730c2cf0556adaae0b9d072157f44de205465a.

--- EECloud.API/Custom stuff/Command/HostCommandSender.vb
+++ EECloud.API/Custom stuff/Command/HostCommandSender.vb
@@ -1,14 +1,16 @@
 ﻿Public Class HostCommandSender
-    Implements ICommandSender
+    Inherits CommandSender
 
 
 #Region "Methods"
     Public Sub New()
-
+        MyBase.New(CommandSenderType.Host)
     End Sub
 
-    Public Sub Reply(msg As String) Implements ICommandSender.Reply
+    Public Overrides Sub Reply(msg As String)
         Cloud.Logger.Log(LogPriority.Info, msg)
     End Sub
 #End Region
+
+
 End Class
