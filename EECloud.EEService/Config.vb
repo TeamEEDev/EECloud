2ed53f764e458d6dbb9c68e79047894d249e46c3
Jojatekok <jojatekokweb@gmail.com>
2013-08-01 10:12:38 +0200

    Started to separate EEService

---
+++ EECloud.EEService/Config.vb
@@ -0,0 +1,7 @@
+﻿Module Config
+
+    ' The string used to connect to the MySQL database where the application's settings are stored.
+    ' If you don't want to use MySQL, just leave this field's value Nothing
+    Friend Const MySQLConnStr As String = Nothing
+
+End Module
---
+++ EECloud.EEService/My Project/AssemblyInfo.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EECloud.EEService Beta")> 
+
+' Setting ComVisible to false makes the types in this assembly not visible
+' to COM components. If you need to access a type in this assembly from
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)> 
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4AFF0ED6-E439-4C8E-8A88-EB5702CB1366")> 
+
+<Assembly: InternalsVisibleTo("EECloud.Host")> 
