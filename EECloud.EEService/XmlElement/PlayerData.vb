0e009e9e9ed4fda36f60713f8848b5666fa0d4ef
Jojatekok <jojatekokweb@gmail.com>
2013-08-01 16:39:23 +0200

    Fixed a concept fail
    Now, any custom game can be easily implemented!

---
+++ EECloud.EEService/XmlElement/PlayerData.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Xml.Serialization
+
+Namespace EECloud.EEService.XmlElement
+    <XmlRoot("PlayerData")>
+    Public Class PlayerData
+        Public Sub New()
+
+        End Sub
+
+
+    End Class
+End Namespace
