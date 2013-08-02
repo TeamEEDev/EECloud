3a11d89b702a789c2edb00f25b9e7c6dff5bfd91
Jojatekok <jojatekokweb@gmail.com>
2013-08-02 11:32:07 +0200

    Fixed an error; added a place for third-party licenses

--- EECloud.EEService/My Project/Resources.Designer.vb
+++ EECloud.EEService/My Project/Resources.Designer.vb
@@ -59,15 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Byte[].
-        '''</summary>
-        Friend ReadOnly Property SQLite_Interop() As Byte()
-            Get
-                Dim obj As Object = ResourceManager.GetObject("SQLite_Interop", resourceCulture)
-                Return CType(obj,Byte())
-            End Get
-        End Property
     End Module
 End Namespace
